c39ffa3151e0e92c13f5f650fe20a09f5073ac84
Kanade <KanadeSupreme@gmail.com>
2025-04-27 00:33:50 +0700

    Feature: Navigate to setup page when space is not set

--- src/renderer/src/routes/__root.tsx
+++ src/renderer/src/routes/__root.tsx
@@ -1,14 +1,26 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useNavigate } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import "@/assets/styles/fonts.css";
 import "@/assets/styles/tailwind.css";
 import "@/assets/styles/common.css";
+import { useEffect } from "react";
+import { useConfigStore } from "@/stores/configStore";
 
 export const Route = createRootRoute({
-   component: () => (
+   component: Root,
+});
+
+function Root() {
+   const { config } = useConfigStore();
+   const navigate = useNavigate();
+   useEffect(() => {
+      if (config.spacePath) return;
+      navigate({ to: "/setup" });
+   }, [config.spacePath, navigate]);
+   return (
       <>
          <Outlet />
          <TanStackRouterDevtools />
       </>
-   ),
-});
+   );
+}
---
+++ src/renderer/src/routes/setup.tsx
@@ -0,0 +1,63 @@
+import { IconButton } from "@/components/MyButton/IconButton";
+import { MyButton } from "@/components/MyButton/MyButton";
+import { MyInput } from "@/components/MyInput/MyInput";
+import { openFileOrDirDialog } from "@/services/dialog";
+import { useConfigStore } from "@/stores/configStore";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { Folder } from "lucide-react";
+import { JSX, useState } from "react";
+
+export const Route = createFileRoute("/setup")({
+   component: SetupPage,
+});
+
+function SetupPage(): JSX.Element {
+   const [filePath, setFilePath] = useState("");
+   const navigate = useNavigate();
+   const { config, setConfig } = useConfigStore();
+   const handleChooseDir = async () => {
+      const res = await openFileOrDirDialog("dir");
+      if (res.canceled) return;
+      const choosedDirPath = res.filePaths[0];
+      setFilePath(choosedDirPath);
+   };
+   return (
+      <div className="center h-screen w-screen flex">
+         <div className="border border-accent rounded-md p-3 min-w-[400px] space-y-3">
+            <div className="flex items-center gap-3">
+               <Folder className="w-5 h-5" />
+               <h1 className="font-bold text-lg"> Choose your space </h1>
+            </div>
+            <p className="text-gray-400">
+               Choose the folder which your books are located
+            </p>
+            <div className="flex items-center gap-2 mt-4">
+               <MyInput
+                  readOnly
+                  className="focus-visible:border-input"
+                  value={filePath}
+               />
+               <IconButton
+                  className="w-9 h-9 shrink-0 bg-accent/40 hover:bg-accent/80 rounded-sm"
+                  onClick={handleChooseDir}
+               >
+                  <Folder className="w-4 h-4" />
+               </IconButton>
+            </div>
+            <div className="flex items-center gap-2 justify-end mt-4">
+               <MyButton
+                  variant="default"
+                  className="font-semibold bg-gray-300 text-gray-800 hover:bg-gray-300/80 w-full"
+                  disabled={!filePath}
+                  onClick={() => {
+                     setConfig({ ...config, spacePath: filePath });
+                     navigate({ to: "/" });
+                  }}
+               >
+                  Start
+               </MyButton>
+            </div>
+         </div>
+      </div>
+   );
+}
