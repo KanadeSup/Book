44102abb328a37d38728e4e331a400fd5fda446f
Kanade <KanadeSupreme@gmail.com>
2025-06-08 22:07:35 +0700

    Refactor: Organize function declaration for easy to read

--- src/renderer/src/components/PDF/PDFViewer.tsx
+++ src/renderer/src/components/PDF/PDFViewer.tsx
@@ -66,20 +66,6 @@ export function PDFViewer() {
       [isLoaded],
    );
 
-   //Persist the position of page when zoom (page scale changes)
-   useEffect(() => {
-      if (currentPageScale.scalePercentage && !previousScaleRef.current) {
-         previousScaleRef.current = currentPageScale.scalePercentage / 100;
-         return;
-      }
-      if (!isLoaded || !scrollElement) return;
-      const currentPageScaleValue = currentPageScale.scalePercentage / 100;
-      scrollElement.scrollTop =
-         (scrollElement.scrollTop * currentPageScaleValue) /
-         previousScaleRef.current;
-      previousScaleRef.current = currentPageScaleValue;
-   }, [currentPageScale.scalePercentage]);
-
    // calculate number of rows
    const numRows = useMemo(() => {
       if (pageLayout === "single-page") {
@@ -142,6 +128,20 @@ export function PDFViewer() {
          handleZoom(scrollAmountTotal.current < 0);
       }
    };
+
+   // Persist the position of page when zoom (page scale changes)
+   useEffect(() => {
+      if (currentPageScale.scalePercentage && !previousScaleRef.current) {
+         previousScaleRef.current = currentPageScale.scalePercentage / 100;
+         return;
+      }
+      if (!isLoaded || !scrollElement) return;
+      const currentPageScaleValue = currentPageScale.scalePercentage / 100;
+      scrollElement.scrollTop =
+         (scrollElement.scrollTop * currentPageScaleValue) /
+         previousScaleRef.current;
+      previousScaleRef.current = currentPageScaleValue;
+   }, [currentPageScale.scalePercentage]);
 
    // Handle update current page when scroll
    const handleScroll = throttle((event: React.UIEvent<HTMLDivElement>) => {
